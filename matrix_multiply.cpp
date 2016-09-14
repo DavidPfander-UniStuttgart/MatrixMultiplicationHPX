98bdd49d3314651b2519b6517cdbef141c5f26b7
pfandedd <pfandedd@ipvs.uni-stuttgart.de>
2016-09-14 18:28:21 -0500

    Still debugging

--- matrix_multiply.cpp
+++ matrix_multiply.cpp
@@ -33,116 +33,123 @@
 
 int hpx_main(boost::program_options::variables_map& vm) {
 
-  // extract command line argument
-  std::uint64_t n = vm["n-value"].as<std::uint64_t>();
-  size_t small_block_size = vm["small-block-size"].as<std::uint64_t>();
-  uint64_t verbose = vm["verbose"].as<uint64_t>();
-  bool check = vm["check"].as<bool>();
-  std::string algorithm = vm["algorithm"].as<std::string>();
+	// extract command line argument
+	std::uint64_t n = vm["n-value"].as<std::uint64_t>();
+	size_t small_block_size = vm["small-block-size"].as<std::uint64_t>();
+	uint64_t verbose = vm["verbose"].as<uint64_t>();
+	bool check = vm["check"].as<bool>();
+	std::string algorithm = vm["algorithm"].as<std::string>();
 
-  std::vector<double> A;
-  std::vector<double> B;
-  std::vector<double> C;
+	std::vector<double> A;
+	std::vector<double> B;
+	std::vector<double> C;
 
-  // create matrices A, B
-  std::default_random_engine generator;
-  std::uniform_real_distribution<double> distribution(0.0, 1.0);
-  auto myRand = std::bind(distribution, generator);
+	// create matrices A, B
+	std::default_random_engine generator;
+	std::uniform_real_distribution<double> distribution(0.0, 1.0);
+	auto myRand = std::bind(distribution, generator);
 
-  A.resize(n * n);
-  std::generate(A.begin(), A.end(), myRand);
+	A.resize(n * n);
+	std::generate(A.begin(), A.end(), myRand);
 
-  if (verbose >= 2) {
-    std::cout << "matrix A:" << std::endl;
-    print_matrix(n, A);
-  }
+	if (verbose >= 2) {
+		std::cout << "matrix A:" << std::endl;
+		print_matrix(n, A);
+	}
 
-  B.resize(n * n);
+	B.resize(n * n);
 
-  for (uint64_t i = 0; i < n; i++) {
-    for (uint64_t j = 0; j < n; j++) {
-      if (i == j) {
-	B.at(i * n + j) = 2.0;
-      } else {
-	B.at(i * n + j) = 0.0;
-      }
-    }
-  }
+	for (uint64_t i = 0; i < n; i++) {
+		for (uint64_t j = 0; j < n; j++) {
+			if (i == j) {
+				B.at(i * n + j) = 2.0;
+			} else {
+				B.at(i * n + j) = 0.0;
+			}
+		}
+	}
 
-  if (verbose >= 2) {
-    std::cout << "matrix B:" << std::endl;
-    print_matrix(n, B);
-  }
+	if (verbose >= 2) {
+		std::cout << "matrix B:" << std::endl;
+		print_matrix(n, B);
+	}
 
-  //    C.resize(n * n);
+	//    C.resize(n * n);
 
-  // Keep track of the time required to execute.
-  hpx::util::high_resolution_timer t;
+	// Keep track of the time required to execute.
+	hpx::util::high_resolution_timer t;
 
-  if (algorithm.compare("single") == 0) {
-    hpx::cout << "using parallel single node algorithm" << std::endl << hpx::flush;
-    hpx::components::client<matrix_multiply_multiplier> multiplier = hpx::new_<
-      hpx::components::client<matrix_multiply_multiplier>>(hpx::find_here(),
-							   n, A, B, verbose);
-    uint32_t comp_locality = hpx::naming::get_locality_id_from_id(multiplier.get_id());
-    multiplier.register_as("/multiplier#" + std::to_string(comp_locality));
-    hpx::components::client<matrix_multiply_recursive> recursive = hpx::new_<
-      hpx::components::client<matrix_multiply_recursive>>(hpx::find_here(), small_block_size, verbose);
-    auto f = hpx::async<
-      matrix_multiply_recursive::distribute_recursively_action>(recursive.get_id(), 0, 0, n);
-    C = f.get();
-  } else if (algorithm.compare("static") == 0) {
-    matrix_multiply_static m(n, A, B, small_block_size, verbose);
-    C = m.matrix_multiply();
-  } else if (algorithm.compare("pseudodynamic") == 0) {
-    std::uint64_t min_work_size = vm["min-work-size"].as<std::uint64_t>();
-    std::uint64_t max_work_difference = vm["max-work-difference"].as<std::uint64_t>();
-    double max_relative_work_difference = vm["max-relative-work-difference"].as<double>();
-    
-    matrix_multiply_static_improved m(n, A, B, small_block_size, min_work_size, max_work_difference,
-				      max_relative_work_difference, verbose);
-    C = m.matrix_multiply();
-  }
+	if (algorithm.compare("single") == 0) {
+		hpx::cout << "using parallel single node algorithm" << std::endl
+				<< hpx::flush;
+		hpx::components::client<matrix_multiply_multiplier> multiplier =
+				hpx::new_<hpx::components::client<matrix_multiply_multiplier>>(
+						hpx::find_here(), n, A, B, verbose);
+		uint32_t comp_locality = hpx::naming::get_locality_id_from_id(
+				multiplier.get_id());
+		multiplier.register_as("/multiplier#" + std::to_string(comp_locality));
+		hpx::components::client<matrix_multiply_recursive> recursive =
+				hpx::new_<hpx::components::client<matrix_multiply_recursive>>(
+						hpx::find_here(), small_block_size, verbose);
+		auto f = hpx::async<
+				matrix_multiply_recursive::distribute_recursively_action>(
+				recursive.get_id(), 0, 0, n);
+		C = f.get();
+	} else if (algorithm.compare("static") == 0) {
+		matrix_multiply_static m(n, A, B, small_block_size, verbose);
+		C = m.matrix_multiply();
+	} else if (algorithm.compare("pseudodynamic") == 0) {
+		std::uint64_t min_work_size = vm["min-work-size"].as<std::uint64_t>();
+		std::uint64_t max_work_difference = vm["max-work-difference"].as<
+				std::uint64_t>();
+		double max_relative_work_difference =
+				vm["max-relative-work-difference"].as<double>();
 
-  char const* fmt = "matrixMultiply(n = %1%)\nelapsed time: %2% [s]";
-  std::cout << (boost::format(fmt) % n % t.elapsed()) << std::endl;
+		matrix_multiply_static_improved m(n, A, B, small_block_size,
+				min_work_size, max_work_difference,
+				max_relative_work_difference, verbose);
+		C = m.matrix_multiply();
+	}
 
-  if (verbose >= 2) {
-    std::cout << "matrix C:" << std::endl;
-    print_matrix(n, C);
-  }
+	char const* fmt = "matrixMultiply(n = %1%)\nelapsed time: %2% [s]";
+	std::cout << (boost::format(fmt) % n % t.elapsed()) << std::endl;
 
-  if (check) {
-    hpx::util::high_resolution_timer t2;
-    std::vector<double> Cref = naiveMatrixMultiply(n, A, B);
-    char const* fmt = "naive matMult took %1% [s]";
-    std::cout << (boost::format(fmt) % t2.elapsed()) << std::endl;
+	if (verbose >= 2) {
+		std::cout << "matrix C:" << std::endl;
+		print_matrix(n, C);
+	}
 
-    if (verbose >= 2) {
-      std::cout << "matrix Cref:" << std::endl;
-      print_matrix(n, Cref);
-    }
+	if (check) {
+		hpx::util::high_resolution_timer t2;
+		std::vector<double> Cref = naiveMatrixMultiply(n, A, B);
+		char const* fmt = "naive matMult took %1% [s]";
+		std::cout << (boost::format(fmt) % t2.elapsed()) << std::endl;
 
-    // compare solutions
-    bool ok = std::equal(C.begin(), C.end(), Cref.begin(), Cref.end(),
-			 [](double first, double second) {
-			   //							std::cout << "first: " << first << " second: " << second << std::endl;
-			   if (std::abs(first - second) < 1E-10) {
-			     //								std::cout << "true" << std::endl;
-			     return true;
-			   } else {
-			     //								std::cout << "false" << std::endl;
-			     return false;
-			   }
-			 });
-    if (ok) {
-      std::cout << "check passed" << std::endl;
-    } else {
-      std::cout << "error: check failed!" << std::endl;
-    }
-  }
+		if (verbose >= 2) {
+			std::cout << "matrix Cref:" << std::endl;
+			print_matrix(n, Cref);
+		}
 
-  return hpx::finalize(); // Handles HPX shutdown
+		// compare solutions
+		bool ok = std::equal(C.begin(), C.end(), Cref.begin(), Cref.end(),
+				[](double first, double second) {
+					//							std::cout << "first: " << first << " second: " << second << std::endl;
+					if (std::abs(first - second) < 1E-10) {
+						//								std::cout << "true" << std::endl;
+						return true;
+					} else {
+						//								std::cout << "false" << std::endl;
+						return false;
+					}
+				});
+		if (ok) {
+			std::cout << "check passed" << std::endl;
+		} else {
+			std::cout << "error: check failed!" << std::endl;
+		}
+	}
+
+	return hpx::finalize(); // Handles HPX shutdown
 }
 
 int main(int argc, char* argv[]) {
@@ -164,16 +171,17 @@ int main(int argc, char* argv[]) {
 			"check result against a naive and slow matrix-multiplication implementation")(
 			"algorithm",
 			boost::program_options::value<std::string>()->default_value(
-					"single"), "select algorithm: single, static")
-	  ("min-work-size",
-	   boost::program_options::value<std::uint64_t>()->default_value(256),
-	   "for pseudodynamic scheduling, minimum work package size per node")
-	  ("max-work-difference",
-	   boost::program_options::value<std::uint64_t>()->default_value(10000),
-	   "for pseudodynamic scheduling, maximum tolerated load inbalance in matrix components assigned")
-	  ("max-relative-work-difference",
-	   boost::program_options::value<double>()->default_value(5.0),
-	   "for pseudodynamic scheduling, maximum relative tolerated load inbalance in matrix components assigned, in percent");
+					"single"), "select algorithm: single, static")(
+			"min-work-size",
+			boost::program_options::value<std::uint64_t>()->default_value(256),
+			"for pseudodynamic scheduling, minimum work package size per node")(
+			"max-work-difference",
+			boost::program_options::value<std::uint64_t>()->default_value(
+					10000),
+			"for pseudodynamic scheduling, maximum tolerated load inbalance in matrix components assigned")(
+			"max-relative-work-difference",
+			boost::program_options::value<double>()->default_value(5.0),
+			"for pseudodynamic scheduling, maximum relative tolerated load inbalance in matrix components assigned, in percent");
 
 	// Initialize and run HPX
 	return hpx::init(desc_commandline, argc, argv);
--- matrix_multiply_static_improved.cpp
+++ matrix_multiply_static_improved.cpp
@@ -10,129 +10,173 @@
 #include "matrix_multiply_recursive.hpp"
 #include "matrix_multiply_multiplier.hpp"
 
-void matrix_multiply_static_improved::insert_submatrix(const std::vector<double> &submatrix, const matrix_multiply_work &w) {
-  for (size_t i = 0; i < w.N; i++) {
-      for (size_t j = 0; j < w.N; j++) {
-	// std::cout << (w.x + i) << ", " << (w.y + j) << " val: " << submatrix[i * w.N + j] << std::endl;
-	C[(w.x + i) * N + (w.y + j)] = submatrix[i * w.N + j];
-      }
-  }
+void matrix_multiply_static_improved::insert_submatrix(
+		const std::vector<double> &submatrix, const matrix_multiply_work &w) {
+	for (size_t i = 0; i < w.N; i++) {
+		for (size_t j = 0; j < w.N; j++) {
+			// std::cout << (w.x + i) << ", " << (w.y + j) << " val: " << submatrix[i * w.N + j] << std::endl;
+			C[(w.x + i) * N + (w.y + j)] = submatrix[i * w.N + j];
+		}
+	}
 }
 
-bool matrix_multiply_static_improved::fulfills_constraints(std::vector<uint64_t> &total_work) {
-  auto result = std::minmax(std::begin(total_work), std::end(total_work));
-  uint64_t min_work = *result.first;
-  uint64_t max_work = *result.second;
-  if (max_work - min_work < max_work_difference) {
-    return true;
-  }
-  if (static_cast<double>(max_work - min_work) / static_cast<double>(min_work) < max_relative_work_difference) {
-    return true;
-  }
-  return false;
+bool matrix_multiply_static_improved::fulfills_constraints(
+		std::vector<uint64_t> &total_work) {
+	uint64_t min_work = total_work[0];
+	uint64_t max_work = total_work[0];
+
+	for (size_t i = 1; i < total_work.size(); i++) {
+		if (total_work[i] < min_work) {
+			min_work = total_work[i];
+		}
+		if (total_work[i] > max_work) {
+			max_work = total_work[i];
+		}
+	}
+
+	if (max_work - min_work < max_work_difference) {
+		return true;
+	}
+	if (static_cast<double>(max_work - min_work) / static_cast<double>(min_work)
+			< max_relative_work_difference) {
+		return true;
+	}
+	return false;
 }
 
-std::vector<std::vector<matrix_multiply_work>> matrix_multiply_static_improved::create_schedule(size_t num_localities) {
-  // one slot per locality, stores the work to do
-  std::vector<std::vector<matrix_multiply_work>> all_work(num_localities);
+std::vector<std::vector<matrix_multiply_work>> matrix_multiply_static_improved::create_schedule(
+		size_t num_localities) {
+	// one slot per locality, stores the work to do
+	std::vector<std::vector<matrix_multiply_work>> all_work(num_localities);
 
-  // map: locality -> assigned components
-  std::vector<uint64_t> total_work(num_localities);
+	// map: locality -> assigned components
+	std::vector<uint64_t> total_work(num_localities);
 
-  // initially all work is placed on the first locality
-  all_work[0].emplace_back(0, 0, N);
-  total_work = {0};
-  total_work[0] = N * N;
+	// initially all work is placed on the first locality
+	all_work[0].emplace_back(0, 0, N);
+	total_work[0] = N * N;
 
-  while (!this->fulfills_constraints(total_work)) {
+	size_t temp = 0;
 
-    // give some work from the max element to the min element
-    uint64_t max_work = total_work[0];
-    size_t max_index = 0;
-    uint64_t min_work = total_work[0];
-    size_t min_index = 0;
+	while (!this->fulfills_constraints(total_work)) {
 
-    for (size_t i = 1; i < num_localities; i++) {
-      if (total_work[i] > max_work) {
-	max_work = total_work[i];
-	max_index = i;
-      }
-      if (total_work[i] < min_work) {
-	min_work = total_work[i];
-	min_index = i;
-      }
-    }
+		std::cout << "-----work-distribution:-----" << std::endl;
+		for (size_t i = 0; i < all_work.size(); i++) {
+			std::vector<matrix_multiply_work> &locality_work = all_work[i];
+			std::cout << "locality: " << i << " = ";
+			for (size_t j = 0; j < locality_work.size(); j++) {
+				if (j > 0) {
+					std::cout << ", ";
+				}
+				std::cout << "[" << locality_work[j].N << "]";
+			}
+			std::cout << std::endl;
+		}
+		// give some work from the max element to the min element
+		uint64_t max_work = total_work[0];
+		size_t max_index = 0;
+		uint64_t min_work = total_work[0];
+		size_t min_index = 0;
 
-    // will always succeed, do not have to skip first iteration
-    size_t work_to_balance_index = 0;
-    uint64_t smallest_work_difference = max_work - min_work;
-    for (size_t j = 0; j < all_work[max_index].size(); j++) {
-      matrix_multiply_work &w = all_work[max_index][j];
-      // split work package between both locations
-      uint64_t split_size = (w.N * w.N) / 2;
-      uint64_t work_difference = (max_work - split_size) - (min_work + split_size);
-      if (work_difference < smallest_work_difference) {
-	smallest_work_difference = work_difference;
-	work_to_balance_index = j;
-      }
-    }
-	      
-    matrix_multiply_work w = all_work[max_index][work_to_balance_index];
-    all_work.erase(all_work.begin() + work_to_balance_index);
+		for (size_t i = 1; i < num_localities; i++) {
+			if (total_work[i] > max_work) {
+				max_work = total_work[i];
+				max_index = i;
+			}
+			if (total_work[i] < min_work) {
+				min_work = total_work[i];
+				min_index = i;
+			}
+		}
 
-    size_t n_new = w.N / 2;
-    std::vector<std::tuple<size_t, size_t>> offsets =
-      {{ 0, 0 }, {0 + n_new, 0 }, { 0, 0 + n_new }, { 0 + n_new, 0 + n_new } };
+		// will always succeed, do not have to skip first iteration
+		size_t work_to_balance_index = 0;
+		uint64_t smallest_work_difference = max_work - min_work;
+		for (size_t j = 0; j < all_work[max_index].size(); j++) {
+			matrix_multiply_work &w = all_work[max_index][j];
+			// split work package between both locations
+			uint64_t split_size = (w.N * w.N) / 2;
+			uint64_t work_difference = (max_work - split_size)
+					- (min_work + split_size);
+			if (work_difference < smallest_work_difference) {
+				smallest_work_difference = work_difference;
+				work_to_balance_index = j;
+			}
+		}
 
-    all_work[max_index].emplace_back(w.x + std::get<0>(offsets[0]), w.y + std::get<1>(offsets[0]), n_new);
-    all_work[max_index].emplace_back(w.x + std::get<0>(offsets[1]), w.y + std::get<1>(offsets[1]), n_new);
-    
-    all_work[min_index].emplace_back(w.x + std::get<0>(offsets[2]), w.y + std::get<1>(offsets[2]), n_new);
-    all_work[min_index].emplace_back(w.x + std::get<0>(offsets[3]), w.y + std::get<1>(offsets[3]), n_new);
-  }
-  return all_work;
+		matrix_multiply_work w = all_work[max_index][work_to_balance_index];
+		all_work[max_index].erase(
+				all_work[max_index].begin() + work_to_balance_index);
+
+		size_t n_new = w.N / 2;
+		std::vector<std::tuple<size_t, size_t>> offsets = { { 0, 0 }, { 0
+				+ n_new, 0 }, { 0, 0 + n_new }, { 0 + n_new, 0 + n_new } };
+
+		all_work[max_index].emplace_back(w.x + std::get<0>(offsets[0]),
+				w.y + std::get<1>(offsets[0]), n_new);
+		all_work[max_index].emplace_back(w.x + std::get<0>(offsets[1]),
+				w.y + std::get<1>(offsets[1]), n_new);
+
+		all_work[min_index].emplace_back(w.x + std::get<0>(offsets[2]),
+				w.y + std::get<1>(offsets[2]), n_new);
+		all_work[min_index].emplace_back(w.x + std::get<0>(offsets[3]),
+				w.y + std::get<1>(offsets[3]), n_new);
+
+		temp += 1;
+		if (temp > 5)
+			break;
+	}
+	return all_work;
 }
 
 std::vector<double> matrix_multiply_static_improved::matrix_multiply() {
-  hpx::cout << "using pseudodynamic distributed algorithm" << std::endl << hpx::flush;
-  size_t num_localities = hpx::get_num_localities().get();
-  std::vector<hpx::id_type> all_ids = hpx::find_all_localities();
-  hpx::default_distribution_policy policy = hpx::default_layout(all_ids);
+	hpx::cout << "using pseudodynamic distributed algorithm" << std::endl
+			<< hpx::flush;
+	size_t num_localities = hpx::get_num_localities().get();
+	std::vector<hpx::id_type> all_ids = hpx::find_all_localities();
+	hpx::default_distribution_policy policy = hpx::default_layout(all_ids);
 
-  // one multiplier per node, to avoid additional copies of A and B
-  std::vector<hpx::components::client<matrix_multiply_multiplier>> multipliers = hpx::new_<
-    hpx::components::client<matrix_multiply_multiplier>[]>(policy, num_localities,
-						     N, A, B, verbose).get();
+// one multiplier per node, to avoid additional copies of A and B
+	std::vector<hpx::components::client<matrix_multiply_multiplier>> multipliers =
+			hpx::new_<hpx::components::client<matrix_multiply_multiplier>[]>(
+					policy, num_localities, N, A, B, verbose).get();
 
-  // colocated
-  for (hpx::components::client<matrix_multiply_multiplier> &multiplier: multipliers) {
-    uint32_t comp_locality = hpx::naming::get_locality_id_from_id(multiplier.get_id());
-    multiplier.register_as("/multiplier#" + std::to_string(comp_locality));
-  }
+// colocated
+	for (hpx::components::client<matrix_multiply_multiplier> &multiplier : multipliers) {
+		uint32_t comp_locality = hpx::naming::get_locality_id_from_id(
+				multiplier.get_id());
+		multiplier.register_as("/multiplier#" + std::to_string(comp_locality));
+	}
 
-  std::vector<hpx::future<void>> futures;
+	std::vector<hpx::future<void>> futures;
 
-    std::vector<hpx::components::client<matrix_multiply_recursive>> recursives;
+	std::vector<hpx::components::client<matrix_multiply_recursive>> recursives;
 
-  std::vector<std::vector<matrix_multiply_work>> all_work = this->create_schedule(num_localities);
+	std::vector<std::vector<matrix_multiply_work>> test1 =
+			this->create_schedule(4);
 
-  for (size_t i = 0; i < all_work.size(); i++) {
-    // transmit the work to the processing locality
-    for (matrix_multiply_work &w: all_work[i]) {
-      hpx::components::client<matrix_multiply_recursive> recursive = hpx::new_<
-	hpx::components::client<matrix_multiply_recursive>>(hpx::find_here(), small_block_size, verbose);    
-      hpx::future<std::vector<double>> f = hpx::async<
-	matrix_multiply_recursive::distribute_recursively_action>(recursive.get_id(), w.x, w.y, w.N);    
-      hpx::future<void> g = f.then(hpx::util::unwrapped(
-							[=](std::vector<double> submatrix)
-							{
-							  this->insert_submatrix(submatrix, w);
-							}));
-      futures.push_back(std::move(g));
-      recursives.push_back(std::move(recursive));
-    }
-  }
+	std::vector<std::vector<matrix_multiply_work>> all_work =
+			this->create_schedule(num_localities);
 
-  hpx::wait_all(futures);
-  return C;
+	for (size_t i = 0; i < all_work.size(); i++) {
+		// transmit the work to the processing locality
+		for (matrix_multiply_work &w : all_work[i]) {
+			hpx::components::client<matrix_multiply_recursive> recursive =
+					hpx::new_<hpx::components::client<matrix_multiply_recursive>>(
+							hpx::find_here(), small_block_size, verbose);
+			hpx::future<std::vector<double>> f = hpx::async<
+					matrix_multiply_recursive::distribute_recursively_action>(
+					recursive.get_id(), w.x, w.y, w.N);
+			hpx::future<void> g = f.then(
+					hpx::util::unwrapped([=](std::vector<double> submatrix)
+					{
+						this->insert_submatrix(submatrix, w);
+					}));
+			futures.push_back(std::move(g));
+			recursives.push_back(std::move(recursive));
+		}
+	}
+
+	hpx::wait_all(futures);
+	return C;
 }
