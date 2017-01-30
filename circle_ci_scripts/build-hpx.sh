1462acae795bfe319be5eac2866630e482511300
DavidPfander-UniStuttgart <david.pfander@ipvs.uni-stuttgart.de>
2017-01-30 23:58:08 +0100

    Update build-hpx.sh

--- circle_ci_scripts/build-hpx.sh
+++ circle_ci_scripts/build-hpx.sh
@@ -12,5 +12,6 @@ fi
 mkdir -p hpx/build
 cd hpx/build
 cmake -DBOOST_ROOT=/home/ubuntu/boost_1_63_0_install -DHPX_WITH_EXAMPLES=OFF -DHPX_WITH_DATAPAR_VC=true -DVc_ROOT=/home/ubuntu/Vc_install -DCMAKE_INSTALL_PREFIX=/home/ubuntu/hpx_install ../
-make -j4 install
+# uses more than 4G with 4 threads (4G limit on Circle CI)
+make -j2 install
 cd ../..
