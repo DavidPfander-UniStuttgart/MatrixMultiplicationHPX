48edb2b671c4daee311d23e4e800f8d8e7b78da6
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-02-01 10:45:47 +0100

    Circle

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -7,18 +7,22 @@
 
 #include "tests.hpp"
 
-
 void start_hpx_with_threads(size_t threads) {
-  std::stringstream s;
-  s << "--hpx:threads=" << threads;
+  std::stringstream s_threads;
+  s_threads << "--hpx:threads=" << threads;
 #ifdef DISABLE_BIND_FOR_CIRCLE_CI
-  s << " --hpx:bind=none";
+  std::stringstream s_bind;
+  s_bind << " --hpx:bind=none";
 #endif
-  std::string hpx_threads(s.str());
+  std::string hpx_threads(s_threads.str());
   // std::string hpx_threads = "--hpx:threads=4";
   std::vector<char *> argv_hpx;
   argv_hpx.push_back(boost::unit_test::framework::master_test_suite().argv[0]);
   argv_hpx.push_back(const_cast<char *>(hpx_threads.c_str()));
+#ifdef DISABLE_BIND_FOR_CIRCLE_CI
+  std::string hpx_bind(s_bind.str());
+  argv_hpx.push_back(const_cast<char *>(hpx_bind.c_str()));
+#endif
   char **argv_hpx_ptr = argv_hpx.data();
-  hpx::start(2, argv_hpx_ptr);
+  hpx::start(3, argv_hpx_ptr);
 }
