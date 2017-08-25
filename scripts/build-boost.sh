47675f968159d58d5fb851a331a0a5525de6e618
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-08-25 13:04:11 +0200

    Circle CI fix

--- scripts/build-boost.sh
+++ scripts/build-boost.sh
@@ -3,8 +3,8 @@ set -x
 set -e
 
 if [[ ! $PARALLEL_BUILD ]]; then
-    echo "PARALLEL_BUILD not set, defaulting to 1"
-    export PARALLEL_BUILD=1
+    echo "boost: PARALLEL_BUILD not set, defaulting to 2"
+    export PARALLEL_BUILD=2
 fi
 
 if [ ! -d "boost_1_65_0/" ]; then
@@ -20,7 +20,7 @@ if [ ! -d "boost_1_65_0_install/" ]; then
     cd boost_1_65_0
     ./bootstrap.sh --prefix="$Boost_ROOT" > bootstrap_boost.log 2>&1
     #-d2: more verbose output
-    ./b2 -j${PARALLEL_BUILD} cxxflags="$CXX_FLAGS" variant=release -d2 install > b2_boost.log 2>&1
+    ./b2 -j${PARALLEL_BUILD} cxxflags="$CXX_FLAGS" variant=release -d1 install > b2_boost.log 2>&1
     cd ..
 fi
 
