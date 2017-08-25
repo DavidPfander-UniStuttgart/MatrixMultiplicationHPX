1a04d2c9e528a75c72392a88138ab1515d07544f
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-08-25 16:19:17 +0200

    Circle CI fix

--- scripts/build-hpx.sh
+++ scripts/build-hpx.sh
@@ -25,7 +25,8 @@ cd hpx/build
 echo "building hpx"
 
 # detection of Vc doesn't work with a relative path
-cmake -DBOOST_ROOT="$Boost_ROOT" -DHPX_WITH_CXX14=ON -DHPX_WITH_EXAMPLES=OFF -DHPX_WITH_DATAPAR_VC=ON -DHPX_WITH_DATAPAR_VC_NO_LIBRARY=ON -DVc_ROOT="$Vc_ROOT" -DCMAKE_INSTALL_PREFIX="$HPX_ROOT" -DJEMALLOC_ROOT=${JEMALLOC_ROOT} -DHPX_WITH_MALLOC=jemalloc -DCMAKE_CXX_STANDARD_LIBRARIES="-latomic" -DCMAKE_BUILD_TYPE=release ../ > cmake_hpx.log 2>&1
+#  > cmake_hpx.log 2>&1
+cmake -DBOOST_ROOT="$Boost_ROOT" -DHPX_WITH_CXX14=ON -DHPX_WITH_EXAMPLES=OFF -DHPX_WITH_DATAPAR_VC=ON -DHPX_WITH_DATAPAR_VC_NO_LIBRARY=ON -DVc_ROOT="$Vc_ROOT" -DCMAKE_INSTALL_PREFIX="$HPX_ROOT" -DJEMALLOC_ROOT=${JEMALLOC_ROOT} -DHPX_WITH_MALLOC=jemalloc -DCMAKE_CXX_STANDARD_LIBRARIES="-latomic" -DCMAKE_BUILD_TYPE=release ../
 
 # uses more than 4G with 4 threads (4G limit on Circle CI)
 # not using a logfile as Circle CI complains about no output for 10 minutes
