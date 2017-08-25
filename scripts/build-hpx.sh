fc51eee99eb9c3246179a6e609a0d5420516b841
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-08-25 13:53:04 +0200

    Circle CI fix

--- scripts/build-hpx.sh
+++ scripts/build-hpx.sh
@@ -28,5 +28,8 @@ echo "building hpx"
 cmake -DBOOST_ROOT="$Boost_ROOT" -DHPX_WITH_CXX14=ON -DHPX_WITH_EXAMPLES=OFF -DHPX_WITH_DATAPAR_VC=ON -DHPX_WITH_DATAPAR_VC_NO_LIBRARY=ON -DVc_ROOT="$Vc_ROOT" -DCMAKE_INSTALL_PREFIX="$HPX_ROOT" -DJEMALLOC_ROOT=${JEMALLOC_ROOT} -DHPX_WITH_MALLOC=jemalloc -DCMAKE_CXX_STANDARD_LIBRARIES="-latomic" -DCMAKE_BUILD_TYPE=release ../ > cmake_hpx.log 2>&1
 
 # uses more than 4G with 4 threads (4G limit on Circle CI)
-make -j${PARALLEL_BUILD} VERBOSE=1 install > make_install_hpx.log 2>&1
+# not using a logfile as Circle CI complains about no output for 10 minutes
+#  > make_install_hpx.log 2>&1
+# VERBOSE=1
+make -j${PARALLEL_BUILD}  install
 cd ../..
