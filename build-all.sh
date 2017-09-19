4331abde6d646d957c90aef2a3e0db0a4abbb3ad
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-09-19 15:29:33 +0200

    added missing file

---
+++ build-all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+set -e
+
+if [ -z "$1" ]; then
+    echo "no target machine specified, building for native"
+    export MATRIX_MULTIPLICATION_TARGET="native"    
+elif [ "$1" = "knl" ]; then
+    export MATRIX_MULTIPLICATION_TARGET="knl"
+elif [ "$1" = "circle" ]; then
+    export MATRIX_MULTIPLICATION_TARGET="circle"
+fi
+
+./scripts/build-jemalloc.sh
+./scripts/build-boost.sh
+./scripts/build-Vc.sh
+./scripts/build-hpx.sh
+./scripts/build-AutoTuneTMP.sh
+./build-MatrixMultiplicationHPX.sh
